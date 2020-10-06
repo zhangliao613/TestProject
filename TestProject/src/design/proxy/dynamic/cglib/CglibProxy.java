e1553762c2c399635b4e1acfed3cbb514db7290f
zhangliao613 <zhangliao613@163.com>
2020-10-06 21:54:35 +0800

    cglib

---
+++ TestProject/src/design/proxy/dynamic/cglib/CglibProxy.java
@@ -0,0 +1,41 @@
+package design.proxy.dynamic.cglib;
+
+import java.lang.reflect.Method;
+
+import design.proxy.dynamic.RealSubject;
+import design.proxy.dynamic.Subject;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+public class CglibProxy implements MethodInterceptor {
+    private Object target;//需要代理的目标对象
+    
+    //重写拦截方法
+    @Override
+    public Object intercept(Object obj, Method method, Object[] arr, MethodProxy proxy) throws Throwable {
+        System.out.println("Cglib动态代理，监听开始！");
+        Object invoke = method.invoke(target, arr);//方法执行，参数：target 目标对象 arr参数数组
+        System.out.println("Cglib动态代理，监听结束！");
+        return invoke;
+    }
+    //定义获取代理对象方法
+    public Object getCglibProxy(Object objectTarget){
+        //为目标对象target赋值
+        this.target = objectTarget;
+        Enhancer enhancer = new Enhancer();
+        //设置父类,因为Cglib是针对指定的类生成一个子类，所以需要指定父类
+        enhancer.setSuperclass(objectTarget.getClass());
+        enhancer.setCallback(this);// 设置回调 
+        Object result = enhancer.create();//创建并返回代理对象
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        CglibProxy cglib = new CglibProxy();//实例化CglibProxy对象
+        Subject subject =  (Subject) cglib.getCglibProxy(new RealSubject());
+        subject.hello("zhangsan");
+        subject.bye();
+    }
+    
+}
